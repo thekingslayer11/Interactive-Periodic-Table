24288d866a60f11da1308b2678011bff19a81bad
TheKingslayer11 <ashifhussain609@gmail.com>
2024-04-06 11:48:57 +0530

    Contents

---
+++ src/ash/Asif.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ash;
+/**
+ *
+ * @author student
+ */
+public class Asif {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new intro().setVisible(true);
+        // TODO code application logic here
+    }
+}
